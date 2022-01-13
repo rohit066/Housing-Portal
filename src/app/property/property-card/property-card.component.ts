097def103bc1652482e979c41878b91801b967e0
Rohit <rohitc200@gmail.com>
2022-01-13 12:00:25 +0530

    Initial Project

---
+++ src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,18 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector:"app-property-card",
+  templateUrl: './property-card.component.html',
+  styleUrls: ['./property-card.component.css']
+})
+
+
+export class PropertyCardComponent{
+  Property: any={
+    "id":1,
+    "Name":"Aditya Birla",
+    "Type":"House",
+    "Price":4000
+  }
+
+}
